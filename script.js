5cafbe09ca6a548fd68e885130bd599936fc0906
Ahmed <mohammadahmed940@gmail.com>
2023-02-11 20:11:50 +0500

    changed from sessionStorage to localStorage

--- script.js
+++ script.js
@@ -9,93 +9,92 @@ fade.onclick = showHideForm;
 submit.onclick = addNewBook;
 
 class bookData {
-    constructor(title, author, pages, haveRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.haveRead = haveRead;
-    }
+  constructor(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+  }
 }
 
 function showHideForm() {
-    form.classList.toggle("show");
-    fade.classList.toggle("show");
+  form.classList.toggle("show");
+  fade.classList.toggle("show");
 }
 
 function removeFunc(index) {
-    myLibrary.splice(index, 1);
-    sessionStorage.setItem("sessionLibrary", JSON.stringify(myLibrary));
-    booksSection.innerHTML = "";
-    myLibrary.forEach(addBook);
+  myLibrary.splice(index, 1);
+  localStorage.setItem("localLibrary", JSON.stringify(myLibrary));
+  booksSection.innerHTML = "";
+  myLibrary.forEach(addBook);
 }
 
 let numberOfRecords = 0;
 function addNewBook() {
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const haveRead = document.querySelector(".checkbox").checked;
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const haveRead = document.querySelector(".checkbox").checked;
 
-    if(title && author && pages) {
-        let sessionLibrary = [];
-        if(sessionStorage.getItem("sessionLibrary"))
-            sessionLibrary = JSON.parse(sessionStorage.getItem("sessionLibrary"));
-        sessionLibrary.push(new bookData(title, author, pages, haveRead));
-        sessionStorage.setItem("sessionLibrary", JSON.stringify(sessionLibrary));
-        showHideForm();
-    }  
+  if (title && author && pages) {
+    let localLibrary = [];
+    if (localStorage.getItem("localLibrary"))
+      localLibrary = JSON.parse(localStorage.getItem("localLibrary"));
+    localLibrary.push(new bookData(title, author, pages, haveRead));
+    localStorage.setItem("localLibrary", JSON.stringify(localLibrary));
+    showHideForm();
+  }
 }
 
-let myLibrary = JSON.parse(sessionStorage.getItem("sessionLibrary"));
+let myLibrary = JSON.parse(localStorage.getItem("localLibrary"));
 myLibrary.forEach(addBook);
 function addBook(bookData, index) {
-
-    function haveReadFunc() {
-        if(bookData.haveRead) {
-            haveRead.style.color = "yellowgreen";
-            return "Have read";
-        }else{
-            haveRead.style.color = "tomato";
-            return "Haven't read";
-        }
+  function haveReadFunc() {
+    if (bookData.haveRead) {
+      haveRead.style.color = "yellowgreen";
+      return "Have read";
+    } else {
+      haveRead.style.color = "tomato";
+      return "Haven't read";
     }
+  }
 
-    const bookCard = document.createElement("div");
-    bookCard.className = "bookCard"
-    
-    const buttons = document.createElement("div");
-    buttons.className = "buttons";
+  const bookCard = document.createElement("div");
+  bookCard.className = "bookCard";
 
-    const toggle = document.createElement("img");
-    toggle.className = "toggle";
-    toggle.src = "images/toggle.png";
-    toggle.onclick = () => {
-        bookData.haveRead = !bookData.haveRead;
-        sessionStorage.setItem("sessionLibrary", JSON.stringify(myLibrary));
-        haveRead.innerHTML = haveReadFunc();
-    };
+  const buttons = document.createElement("div");
+  buttons.className = "buttons";
 
-    const remove = document.createElement("img");
-    remove.className = "remove";
-    remove.src = "images/remove.png";
-    remove.onclick = () => removeFunc(index);
-    
-    const title = document.createElement("h2");
-    const author = document.createElement("h3");
-    const pages = document.createElement("h3");
-    const haveRead = document.createElement("h3");
-
-    title.innerHTML = bookData.title;
-    author.innerHTML = "by " + bookData.author;
-    pages.innerHTML = bookData.pages + " pages";
+  const toggle = document.createElement("img");
+  toggle.className = "toggle";
+  toggle.src = "images/toggle.png";
+  toggle.onclick = () => {
+    bookData.haveRead = !bookData.haveRead;
+    localStorage.setItem("localLibrary", JSON.stringify(myLibrary));
     haveRead.innerHTML = haveReadFunc();
-    
-    buttons.appendChild(toggle);
-    buttons.appendChild(remove);
-    bookCard.appendChild(buttons);
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(haveRead);
-    booksSection.appendChild(bookCard);
+  };
+
+  const remove = document.createElement("img");
+  remove.className = "remove";
+  remove.src = "images/remove.png";
+  remove.onclick = () => removeFunc(index);
+
+  const title = document.createElement("h2");
+  const author = document.createElement("h3");
+  const pages = document.createElement("h3");
+  const haveRead = document.createElement("h3");
+
+  title.innerHTML = bookData.title;
+  author.innerHTML = "by " + bookData.author;
+  pages.innerHTML = bookData.pages + " pages";
+  haveRead.innerHTML = haveReadFunc();
+
+  buttons.appendChild(toggle);
+  buttons.appendChild(remove);
+  bookCard.appendChild(buttons);
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(haveRead);
+  booksSection.appendChild(bookCard);
 }
